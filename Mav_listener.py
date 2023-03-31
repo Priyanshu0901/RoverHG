8c834d61a133095fbe43e9c7ffc252d304294795
Priyanshu Roy <roypriyanshu09@gmail.com>
2023-03-31 23:07:31 +0530

    Commit_0.1

---
+++ Mav_listener.py
@@ -0,0 +1,13 @@
+from pymavlink import mavutil
+
+# Start a connection listening on a UDP port
+the_connection = mavutil.mavlink_connection('/dev/ttyACM1')
+
+# Wait for the first heartbeat 
+#   This sets the system and component ID of remote system for the link
+the_connection.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+
+while 1:
+    msg = the_connection.recv_match(type=['RAW_IMU','SCALED_IMU2','ATTITUDE'],blocking=True)
+    print(msg)
