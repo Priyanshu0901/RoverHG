a325c4546afdb32e7e5db6ba4e4306666484346e
Priyanshu Roy <roypriyanshu09@gmail.com>
2023-04-03 01:35:26 +0530

    ROS_Installation

---
+++ ROS2_v_0_0/src/Rover_Controller/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'Rover_Controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rayv',
+    maintainer_email='roypriyanshu09@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
